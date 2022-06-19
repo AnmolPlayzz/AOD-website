ab45d3a9f0cbb9a0974f31288f45adbc162032f9
AnmolPlayzz <74979911+AnmolPlayzz@users.noreply.github.com>
2022-06-19 14:53:53 +0530

    update: fix index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,3 +5,10 @@ $: if (browser) {
   goto(`/home`);
 }
 </script>
+
+<svelte:head>
+<meta name="description" content="Check out my website!">
+<title>Art of Dragons - Home</title>
+<meta property="og:image" content="/round.png">
+<link rel="icon" href="/round.png" type="image/x-icon">
+</svelte:head>
