a540ffea273a23eb9c6b168f3fdc1cd20e6bc58f
AnmolPlayzz <74979911+AnmolPlayzz@users.noreply.github.com>
2022-06-23 13:25:20 +0530

    feat: about page

---
+++ src/routes/about.md
@@ -0,0 +1,11 @@
+<br>
+<br>
+<div class="div">
+    
+</div>
+
+<style>
+    .div {
+        margin: 40px
+    }
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,28 @@
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
 import adapter from '@sveltejs/adapter-auto';
 import sveltePreprocess from 'svelte-preprocess';
-
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()	
+		adapter: adapter()
 	}
-	
 };
 
 export default {
-    preprocess: sveltePreprocess(),
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+	preprocess: [sveltePreprocess(), mdsvex(mdsvexConfig)],
+
 	server: {
 		fsServe: {
-		  root: '../' 
+			root: '../'
 		}
-	  },
+	},
+
 	ssr: false,
+
 	kit: {
-		adapter: adapter()	
+		adapter: adapter()
 	}
 };
